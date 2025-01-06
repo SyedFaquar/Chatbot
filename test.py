d3383e500921c37ce164326f92497bb5b26aeafc
SyedFaquar <squadri000@citymail.cuny.edu>
2025-01-05 22:56:32 -0500

    tried OpenAI, Anthropic API wiht langchain

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import os
 
 
+def main():
+    api_key = os.getenv("OPENAI_API_KEY")
+
+    if api_key is None:
+        print("OPENAI_API_KEY is not set")
+    else:
+        print("OPENAI_API_KEY is set")
+        print(api_key)
+
+if __name__ == "__main__":
+    main()
+        
